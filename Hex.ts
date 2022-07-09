ce06f8b5bd9b558d9bea8a6a2797c851edf2c9b0
Kyle Coberly <kyle.coberly@gmail.com>
2022-07-09 00:42:57 -0600

    Add solution

--- Hex.ts
+++ Hex.ts
@@ -1,2 +1,31 @@
 export default class Hex {
+  decimal!: number
+
+  constructor(decimal: number) {
+    this.decimal = decimal
+  }
+  static parse(hex: string) {
+    return parseInt(hex, 16)
+  }
+  private static parseHex(decimal: number): string {
+    return `0x${decimal.toString(16).toUpperCase()}`
+  }
+
+  minus(amount: number) {
+    this.decimal -= amount
+    return this
+  }
+  plus(amount: number) {
+    this.decimal += amount
+    return this
+  }
+  toString() {
+    return Hex.parseHex(this.decimal)
+  }
+  toJSON() {
+    return JSON.stringify(this.toString())
+  }
+  valueOf() {
+    return this.decimal
+  }
 }
